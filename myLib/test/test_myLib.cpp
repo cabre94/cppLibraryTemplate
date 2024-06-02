953f99181eff34254392732ab7476236e2cd5973
Facundo Martín Cabrera <facundo.cabrera@ptech.com.ar>
2024-06-02 15:15:42 -0300

    Add complex test #1

--- myLib/test/test_myLib.cpp
+++ myLib/test/test_myLib.cpp
@@ -1,25 +1,35 @@
 #include <gtest/gtest.h>
 
-#include "myLib.h"
+#include <myLib.h>
+
+TEST(myLibTests, Test_ComplexConstructor)
+{
+	const myLib::Complex c{4, 20};
+    const double expected_re = 4;
+    const double expected_im = 20;
+
+    ASSERT_EQ(expected_re, c.Re());
+    ASSERT_EQ(expected_im, c.Im());
+}
 
 TEST(myLibTests, TestIntegerOne_One)
 {
     const auto expected = 1;
-    const auto actual = multiply(1, 1);
+    const auto actual = myLib::multiply(1, 1);
     ASSERT_EQ(expected, actual);
 }
 
 TEST(myLibTests, TestIntegerZero_Zero)
 {
     const auto expected = 0;
-    const auto actual = multiply(0, 0);
+    const auto actual = myLib::multiply(0, 0);
     ASSERT_EQ(expected, actual);
 }
 
 TEST(myLibTests, TestIntegerZero_One)
 {
     const auto expected = 0;
-    const auto actual = multiply(0, 1);
+    const auto actual = myLib::multiply(0, 1);
     ASSERT_EQ(actual, expected);
 }
 
