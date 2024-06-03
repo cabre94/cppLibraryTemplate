f7de1adb35d81461179311df4a2cac25c491c26f
Facundo Martín Cabrera <facundo.cabrera@ptech.com.ar>
2024-06-03 10:20:55 -0300

    test #1

--- myLib/test/test_myLib.cpp
+++ myLib/test/test_myLib.cpp
@@ -8,8 +8,25 @@ TEST(myLibTests, Test_ComplexConstructor)
     const double expected_re = 4;
     const double expected_im = 20;
 
-    ASSERT_EQ(expected_re, c.Re());
-    ASSERT_EQ(expected_im, c.Im());
+    ASSERT_DOUBLE_EQ(expected_re, c.Re());
+    ASSERT_DOUBLE_EQ(expected_im, c.Im());
+}
+
+TEST(myLibTests, Test_ComplexBasicOperations)
+{
+	const myLib::Complex c1{4.20, 20.4};
+	const myLib::Complex c2{3, 4};
+
+	const myLib::Complex c_sum = c1 + c2;
+    ASSERT_DOUBLE_EQ(7.20, c_sum.Re());
+    ASSERT_DOUBLE_EQ(24.4, c_sum.Im());
+
+	const myLib::Complex c_sub = c2 - c1;
+	// ASSERT_EQ(-1.20, c_sub.Re());
+    // ASSERT_EQ(-16, c_sub.Im());
+
+	ASSERT_DOUBLE_EQ(-1.20, c_sub.Re());
+    ASSERT_DOUBLE_EQ(-16.4, c_sub.Im());
 }
 
 TEST(myLibTests, TestIntegerOne_One)
